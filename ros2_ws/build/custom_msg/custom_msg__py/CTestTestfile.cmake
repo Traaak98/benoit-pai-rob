dc11706c8b600208c0ec4d010f4fcac8263487b5
Apolline <apolline.de_vaulchier@ensta-bretagne.org>
2023-11-08 15:13:20 +0100

    pink robot

--- ros2_ws/build/custom_msg/custom_msg__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /media/apolline/B2C2CC8EC2CC586D/apolline/Ecole/ENSTA/M2/IS/benoit-pai-rob/ros2_ws/build/custom_msg/custom_msg__py
-# Build directory: /media/apolline/B2C2CC8EC2CC586D/apolline/Ecole/ENSTA/M2/IS/benoit-pai-rob/ros2_ws/build/custom_msg/custom_msg__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- ros2_ws/build/tennis_court/ament_cmake_core/stamps/rosidl_cmake-extras.cmake.stamp
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(tennis_court_IDL_FILES "msg/BallManagerStats.idl")
-set(tennis_court_INTERFACE_FILES "msg/BallManagerStats.msg")
