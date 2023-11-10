dc3d77e348c8424562cfdd9104cacc369e279b5c
gondotcl <clara.gondot@ensta-bretagne.org>
2023-11-10 08:20:48 +0100

    premières modifs

--- ros2_ws/src/benoit_pairob/src/main_nodePilotage.cpp
+++ ros2_ws/src/benoit_pairob/src/main_nodePilotage.cpp
@@ -20,9 +20,11 @@ void PilotageNode::init_parameters() {
     x_ << 0, 0, 0;
     target_ << 0, 0;
     k = 0.5;
-    u1_ = 0;
+    u_ << 0, 0, 0;
     teleop = false;
     button_pressed = false;
+    v_ = 2.;
+    fsm_ = 0;   // attente de la balle
 }
 
 void PilotageNode::set_x(sensor_msgs::msg::Imu pose) {
@@ -67,28 +69,75 @@ void PilotageNode::timer_callback(){
     } else {
         control();
 
-        message.linear.x = 0;
-        message.linear.y = 0.;
-        message.angular.z = u1_;
+        message.linear.x = u_(0);
+        message.linear.y = u_(1);
+        message.angular.z = u_(2);
     }
-
+    RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "commande : %f %f %f", u_(0), u_(1), u_(2));
     publisher_command_->publish(message);
 }
 
 void PilotageNode::control() {
-    //double e = atan2(target_(1)-x_(1), target_(0)-x_(0))-x_(2);
-    double e = target_(0) - x_(2);
-    e = 2*atan(tan(e/2));
+    double e = std::atan2(target_(1), target_(0)) - x_(2);
+    bool ball_presence = target_(0) != 0 || target_(1) != 0;
+    // ball_presence *=
+    RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "bool : %d", ball_presence);
+    bool clara_func = false; // pour l'instant on y va pas
 
-    if (e > M_PI/4)
-    {
-        u1_ = M_PI/5;
-    } else if (e < -M_PI/4)
-    {
-        u1_ = -M_PI/5;
-    } else {
-        u1_ = k*e;
+    if (target_(0) > img_w/2 && x_(0) < img_w/2)  {
+        // CHANGER DE COTE --> regarder en haut ou en bas puis passage
+        clara_func = false;
     }
+    else if (target_(0) < img_w/2 && x_(0) > img_w/2) {
+        // CHANGER DE COTE --> regarder en haut ou en bas
+        clara_func = false;
+    }
+
+    // if ((target_(0) > zone_E[0] && target_(0) < filet_1[0] - 1.5*coef_x && target_(1) < zone_F[1] && target_(1) > zone_D[1])
+    //    || (target_(0) > filet_1[0] + 1.5*coef_x && target_(0) < zone_A[0] && target_(1) < zone_F[1] && target_(1) > zone_D[1])) {
+    if (ball_presence && fsm_ == 1) {
+        // DANS LE CARRE FAIRE SUIVI DE CAP SIMPLE
+        e = 2*atan(tan(e/2));
+        if (e > M_PI/4)
+        {
+            u_(2) = M_PI/5;
+        } else if (e < -M_PI/4)
+        {
+            u_(2) = -M_PI/5;
+        } else {
+            u_(2) = k*e;
+        }
+
+        u_ << 5, 5, u_(2);
+    }
+    else {
+        u_ << 0, 0, u_(2);
+    }
+
+    if (ball_presence && fsm_ == 2) { // FAIRE FONCTION CLARA :  sur les bords :  approche - virage - avance
+        // distinction de cas : mur vertical ou horizontal
+        float eps_x = coef_x * 1.5; // marge
+        float eps_y = coef_y * 1.5;
+        float dist = eps_x;
+        float p_x, p_y; // point de la trajectoire
+
+        if (1280 - target_(0) < eps_x || target_(0) < eps_x) {
+            // mur du bas ou du haut
+            p_x = target_(0) + dist;
+            p_y = target_(1);
+        }
+        else if (720 - target_(1) < eps_y || target_(1) < eps_y) {
+            // mur de gauche ou de droite
+            p_x = target_(0);
+            p_y = target_(1) + dist;
+        }
+
+        u_(2) = std::atan2(p_y - x_(1), p_x - x_(0));
+        bool condition = std::sqrt(std::pow(p_x - x_(0), 2) + std::pow(p_y - x_(1), 2)) < 0.2 * coef_x;
+        // suivre cap jusqu-à ce que condition soit fausse puis suivre cap vers balle
+
+    }
+
 }
 
 /****************************************
