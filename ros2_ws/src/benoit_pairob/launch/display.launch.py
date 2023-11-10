bd86985629a96d95acfb985f1453fa45c92f7c27
gondotcl <clara.gondot@ensta-bretagne.org>
2023-11-10 12:48:18 +0100

    FSM updated

--- ros2_ws/src/benoit_pairob/launch/display.launch.py
+++ ros2_ws/src/benoit_pairob/launch/display.launch.py
@@ -66,19 +66,19 @@ def generate_launch_description():
     )
 
     return launch.LaunchDescription([
-        #spawn_entity,
-        #joint_state_publisher_node,
+        spawn_entity,
+        joint_state_publisher_node,
 
-        #tennis_court_launch,
+        tennis_court_launch,
         launch.actions.DeclareLaunchArgument(name='model', default_value=default_model_path,
                                             description='Absolute path to robot urdf file'),
         robot_state_publisher_node,
         launch.actions.DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path,
                                             description='Absolute path to rviz config file'),
-        #nodeDetectBall,
-        #nodeDetectRobot,
-        #nodeChooseClosest,
-        rviz_node,
-        #joycon_node,
-        #nodePilotage
+        nodeDetectBall,
+        nodeDetectRobot,
+        nodeChooseClosest,
+        #rviz_node,
+        joycon_node,
+        nodePilotage
     ])
